4c99f5f261a604a62d061be92f0f59f1dfd357cd
Dordy175 <ladislavdorotik@gmail.com>
2020-09-29 18:53:10 +0200

    Update Main.java

--- Problems/What's wrong/src/Main.java
+++ Problems/What's wrong/src/Main.java
@@ -8,4 +8,10 @@ public class Main {
 
         System.out.println(number+1);
     }
+
+//    public static void main(String[] args){
+//        char ch = '5';
+//        int ch2 = 6;
+//        System.out.printf("Marks for my assignment is %d%d",ch, ch2);
+//    }
 }
